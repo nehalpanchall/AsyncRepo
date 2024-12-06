7f1fa687c617d74dc82966001099e68b9aa8f746
nehalpanchall <nehalpanchal40@gmail.com>
2024-12-06 01:43:15 -0500

    complete cricket project using functions and ladder if

---
+++ cricket/cricket.js
@@ -0,0 +1,68 @@
+const choice = (val) => {
+  if (val == 1) {
+    return `Bat`;
+  } else if (val == 2) {
+    return `Ball`;
+  } else {
+    return `Stump`;
+  }
+};
+
+const bat = (val) => {
+  let message = `You selected: ${choice(val)}`;
+  var computerVal = Math.ceil(Math.random() * 3);
+  let compMsg = choice(computerVal);
+
+  if (computerVal == val) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} Match Tie!!! `
+    ); // 1
+  } else if (computerVal == 2) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Won.. `
+    );
+  } else {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Lost.. `
+    );
+  }
+};
+const ball = (val) => {
+  let message = `You selected: ${choice(val)}`;
+  var computerVal = Math.ceil(Math.random() * 3);
+  let compMsg = choice(computerVal);
+
+  if (computerVal == val) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} Match Tie!!! `
+    ); // 2
+  } else if (computerVal == 1) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Lost.. `
+    );
+  } else {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Won.. `
+    );
+  }
+};
+const stump = (val) => {
+  let message = `You selected: ${choice(val)}`;
+
+  var computerVal = Math.ceil(Math.random() * 3);
+  let compMsg = choice(computerVal);
+
+  if (computerVal == val) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} Match Tie!!! `
+    ); // 3
+  } else if (computerVal == 2) {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Lost.. `
+    );
+  } else {
+    alert(
+      `${message} ${"\n"}Computer selected: ${compMsg} ${"\n \n"} You Won.. `
+    );
+  }
+};
