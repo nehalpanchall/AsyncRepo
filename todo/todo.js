e4e0377fef863f5a8b17a11899e6dd8495062e9a
nehalpanchall <nehalpanchal40@gmail.com>
2024-12-15 10:26:57 -0500

    push todoName and todoDate into saperate array and display in single div

--- todo/todo.js
+++ todo/todo.js
@@ -4,13 +4,36 @@ let todo_add = document.querySelector("#todo-add");
 
 todo_add.addEventListener("click", addTodo);
 
+// ["Nehal","Pintu","Ninja"] : Length: 3
+
+let todoName = [];
+let todoDate = [];
+
 function addTodo() {
   let todo_display_div = document.querySelector("#todo-display");
   let div = document.createElement("div");
-  div.innerHTML = todo_name.value;
+
+  todoName.push(todo_name.value);
+  todoDate.push(todo_date.value);
+
+  let spanName = document.createElement("span");
+  let spanDate = document.createElement("span");
+
+  for (let i = 0; i < todoName.length; i++) {
+    spanName.innerHTML = todoName[i];
+  }
+
+  for (let i = 0; i < todoDate.length; i++) {
+    spanDate.innerHTML = todoDate[i];
+  }
+
+  div.appendChild(spanName);
+  div.appendChild(spanDate);
   todo_display_div.appendChild(div);
 
+  // Styling part
   todo_name.value = "";
+  todo_date.value = "";
   todo_name.focus();
   todo_display_div.style = "margin-top: 20px";
   div.style = "margin: 10px 0px";
