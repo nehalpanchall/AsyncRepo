96ad50202710ecb2f6e031c110ecca7ba2c59322
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-01 08:57:32 -0500

    write producer part of Promise object with resolve and reject functions

--- promise.js
+++ promise.js
@@ -1,14 +1,37 @@
 const cart = ['A', 'B', 'C'];
 
 let promiseX = createOrder(cart);
+console.log(promiseX);
 
-promiseX
-  .then(function (orderId) {
-    proceedToPay(orderId);
-  })
-  .then(function (paymentInfo) {
-    console.log(paymentInfo);
-  })
-  .catch(function (er) {
-    console.log(er.message);
+// consumer part
+promiseX.then(function (response) {
+  let data = JSON.parse(response);
+  console.log(data); // JS object
+});
+
+// producer part
+function createOrder(cart) {
+  let promise = new Promise(function (resolve, reject) {
+    if (validateCart(cart)) {
+      let orderId = { id: 'xyz123' };
+
+      setTimeout(() => {
+        resolve(JSON.stringify(orderId));
+      }, 5000);
+      // resolve with data in JSON object after 5 seconds
+    } else {
+      let err = new Error('Cart is not valid');
+      reject(err); // reject with error message
+    }
+
+    function validateCart(cart) {
+      let flag = false;
+      if (cart.length > 0) {
+        flag = true;
+      }
+      return flag;
+    }
   });
+
+  return promise;
+}
