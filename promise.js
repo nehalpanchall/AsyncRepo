cc374cf51dd24316dbfdbc4aefefc8c961526fce
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-01 09:59:04 -0500

    create promise for proceedToPay and manage promise rejection flow

--- promise.js
+++ promise.js
@@ -1,4 +1,4 @@
-const cart = [];
+const cart = ['A'];
 
 let promiseX = createOrder(cart);
 // console.log(promiseX);
@@ -6,20 +6,31 @@ let promiseX = createOrder(cart);
 // consumer part
 promiseX
   .then(function (response) {
-    let data = JSON.parse(response);
-    console.log(data); // JS object
+    let orderData = JSON.parse(response);
+    return orderData;
+  })
+  .then(function (orderData) {
+    console.log(orderData); // JS object
+    let payData = proceedToPayment(orderData);
+    return payData;
+  })
+  .then(function (payData) {
+    console.log(payData);
   })
   .catch(function (error) {
     console.log(error.name); // Error
     console.log(error.message); // Cart is not valid
     console.log(error.stack); // Error: Cart is not valid (at line number, at filename, at function)
+  })
+  .then(function () {
+    console.log('Finishedddd');
   });
 
 // producer part
 function createOrder(cart) {
   let promise = new Promise(function (resolve, reject) {
     if (validateCart(cart)) {
-      let orderId = { id: 'xyz123' };
+      let orderId = { id: 'order123' };
 
       setTimeout(() => {
         resolve(JSON.stringify(orderId));
@@ -36,6 +47,27 @@ function createOrder(cart) {
         flag = true;
       }
       return flag;
+    }
+  });
+
+  return promise;
+}
+
+function proceedToPayment(data) {
+  let promise = new Promise(function (resolve, reject) {
+    let flag = true;
+    if (!data) {
+      flag = false;
+    }
+
+    if (!flag) {
+      let paymentId = { id: 'payment123' };
+      setTimeout(() => {
+        resolve(paymentId);
+      }, 3000);
+    } else {
+      let err = new Error('Payment declined!');
+      reject(err);
     }
   });
 
