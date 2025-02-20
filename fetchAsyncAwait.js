6845d30e2c50a688993a07f30c95037dc508dabd
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-19 21:06:05 -0500

    fix: bugs in fetch() uisng async/await

--- fetchAsyncAwait.js
+++ fetchAsyncAwait.js
@@ -24,3 +24,55 @@ async function fetchData() {
 }
 
 fetchData();
+
+// Promise.all() with async/await
+
+async function fetchProducts() {
+  try {
+
+    let response1 = await fetch('https://dummyjson.com/products/1')
+    let data = await response1.json();
+    
+   
+
+
+    let [data1, data2] = await Promise.all([
+
+        
+
+        response1.then(function (response){
+            return response.json()
+        }),
+
+
+        let two = await fetch('https://dummyjson.com/products/2')
+      
+      
+    ]);
+
+    console.log(data1, data2);
+  } catch (error) {
+    console.error(error);
+  }
+}
+fetchProducts();
+
+
+
+
+// original Code
+
+async function fetchProducts() {
+    try {
+      let [data1, data2] = await Promise.all([
+        fetch('https://dummyjson.com/products/1').then(res => res.json()),
+        fetch('https://dummyjson.com/products/2').then(res => res.json())
+      ]);
+  
+      console.log(data1, data2);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  fetchProducts();
+  
