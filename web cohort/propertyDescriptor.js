dbe1dc657dc77d6d00590473dc3d4ea81f1ac3bc
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-24 01:12:23 -0500

    update: enumerable propety of descriptor object using Object.defineProperty()

--- web cohort/propertyDescriptor.js
+++ web cohort/propertyDescriptor.js
@@ -6,10 +6,27 @@ const person = {
   isStudent: true,
 };
 
+// enumerable
+Object.defineProperty(person, 'id', { enumerable: false });
+
+// 1. for..in.. loop
+for (let i in person) {
+  console.log('uisng for..in.. loop: ', i);
+}
+
+// 2. Object.keys()
+console.log('Object.keys(): ', Object.keys(person));
+
+// 3. Object.values()
+console.log(Object.values(person));
+
+// 4. Object.entiries()
+console.log(Object.entries(person));
+
 // writable
-let a = Object.getOwnPropertyDescriptor(person, 'id');
-Object.defineProperty(person, 'id', { writable: false });
+// let a = Object.getOwnPropertyDescriptor(person, 'id');
+// Object.defineProperty(person, 'id', { writable: false });
 
-person.id = 33;
+// person.id = 33;
 
-console.log(person);
+// console.log(person);
