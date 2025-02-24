faea949732d876a3676a0b6b0342e6dc1b6102a9
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-24 09:58:34 -0500

    update: configurable: false and explore the edge cases

--- web cohort/propertyDescriptor.js
+++ web cohort/propertyDescriptor.js
@@ -6,22 +6,30 @@ const person = {
   isStudent: true,
 };
 
+// configuarable
+Object.defineProperty(person, 'id', { configurable: false });
+
+console.log(Object.getOwnPropertyDescriptor(person, 'id'));
+
+// cannot delete (because configurable: false)
+delete person.id;
+
 // enumerable
-Object.defineProperty(person, 'id', { enumerable: false });
+// Object.defineProperty(person, 'id', { enumerable: false });
 
-// 1. for..in.. loop
-for (let i in person) {
-  console.log('uisng for..in.. loop: ', i);
-}
+// // 1. for..in.. loop
+// for (let i in person) {
+//   console.log('uisng for..in.. loop: ', i);
+// }
 
-// 2. Object.keys()
-console.log('Object.keys(): ', Object.keys(person));
+// // 2. Object.keys()
+// console.log('Object.keys(): ', Object.keys(person));
 
-// 3. Object.values()
-console.log(Object.values(person));
+// // 3. Object.values()
+// console.log(Object.values(person));
 
-// 4. Object.entiries()
-console.log(Object.entries(person));
+// // 4. Object.entiries()
+// console.log(Object.entries(person));
 
 // writable
 // let a = Object.getOwnPropertyDescriptor(person, 'id');
