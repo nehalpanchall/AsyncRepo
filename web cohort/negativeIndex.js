282bdbe794e97d5b57e27c45557931f3dd1120d2
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-27 10:16:16 -0500

    feat: create a function to create proxied array from original array

--- web cohort/negativeIndex.js
+++ web cohort/negativeIndex.js
@@ -1,29 +1,42 @@
 let arr = [1, 2, 3, 4, 5];
 
-// Proxied Array
-const proxy = new Proxy(arr, {
-  get(target, prop) {
-    const index = Number(prop);
+function negativeIndex(arr) {
+  return new Proxy(arr, {
+    get(target, prop) {
+      const index = Number(prop);
 
-    if (index < 0) {
-      return target[target.length + index];
-    }
-    return target[index];
-  },
+      if (index < 0) {
+        return target[target.length + index];
+      }
+      return target[index];
+    },
 
-  set(target, prop, value) {
-    const index = Number(prop);
-    if (index < 0) {
-      target[target.length + index] = value; // set to negative index
-    } else {
-      target[index] = value; // set to positive index
-    }
-    return true;
-  },
-});
+    set(target, prop, value) {
+      const index = Number(prop);
+      if (index < 0) {
+        target[target.length + index] = value; // set to negative index
+      } else {
+        target[index] = value; // set to positive index
+      }
+      return true;
+    },
+  });
+}
 
-console.log(proxy[-3]);
+const proxiedArr = negativeIndex(arr);
 
-proxy[-1] = 'Nehal';
-console.log('Proxied Array: ', proxy);
+// get()
+console.log(proxiedArr[-3]);
+
+// set()
+proxiedArr[-2] = 'Nehal';
+proxiedArr[1] = 'Panchal';
+
+console.log('Proxied Array: ', proxiedArr);
 console.log('Original Array: ', arr);
+
+// console.log(proxy[-3]);
+
+// proxy[-1] = 'Nehal';
+// console.log('Proxied Array: ', proxy);
+// console.log('Original Array: ', arr);
