cdf46861f528a38c831423db81aa41a0cbd915c7
nehalpanchall <nehalpanchal40@gmail.com>
2025-02-28 01:12:03 -0500

    add: setup files and layout for dynamic list of blogs

---
+++ Blogs/dropdown.js
@@ -0,0 +1,10 @@
+const buttonElement = document.querySelector('#buttonElement');
+
+function addTtitle() {
+  alert('Button clicked!');
+}
+
+function textChanged() {
+  const textElement = document.querySelector('#textElement');
+  console.log(textElement.value);
+}
